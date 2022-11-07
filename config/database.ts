9901caf234a657db7082be1b38413b21bf320a4e
Alex Ialanji <alexandr.ialanji@codefactorygroup.com>
2022-11-07 14:42:38 +0200

    try

--- config/database.ts
+++ config/database.ts
@@ -1,15 +1,17 @@
 import path from 'path';
 
+const parse = require('pg-connection-string').parse;
+const config = parse("postgres://gzdmqltdmlyyqq:fd29d7b911a3dc6fb27ea57ecd6815b2c85fa2d7aac89814cd0f2d77e6762782@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d1esslnld03s0i");
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-176-34-215-248.eu-west-1.compute.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd1esslnld03s0i'),
-      user: env('DATABASE_USERNAME', 'gzdmqltdmlyyqq'),
-      password: env('DATABASE_PASSWORD', 'fd29d7b911a3dc6fb27ea57ecd6815b2c85fa2d7aac89814cd0f2d77e6762782'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
         rejectUnauthorized: false
       },
