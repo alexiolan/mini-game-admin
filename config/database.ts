078e093de28ed566e9d61ea8a7c0824cd271cbc9
Alex Ialanji <alexandr.ialanji@codefactorygroup.com>
2022-11-07 14:58:25 +0200

    CONFIGS

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 const parse = require('pg-connection-string').parse;
-const config = parse("postgres://gzdmqltdmlyyqq:fd29d7b911a3dc6fb27ea57ecd6815b2c85fa2d7aac89814cd0f2d77e6762782@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d1esslnld03s0i");
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
